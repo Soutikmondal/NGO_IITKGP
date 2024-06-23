d170a08bb230f78fff121bce42c6cfcc3dcf4a88
Soutikmondal <69642549+Soutikmondal@users.noreply.github.com>
2024-06-23 13:43:53 +0530

    removed api key

--- lib/Chat_Bot/constants/api_consts.dart
+++ lib/Chat_Bot/constants/api_consts.dart
@@ -1,5 +1,5 @@
 const String BASE_URL = "https://generativelanguage.googleapis.com/v1beta";
-const String API_KEY = "AIzaSyBPfMkdpwB5CH2rNjTIzyrQ2wVjpSajB1E";
+const String API_KEY = "APIKEY";
 
 
 //AIzaSyBPfMkdpwB5CH2rNjTIzyrQ2wVjpSajB1E
